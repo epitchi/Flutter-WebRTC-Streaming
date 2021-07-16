e2eb141bd307d70fe15ef286d555c4cca23fd56e
Lê Văn Thiện <52475801+epitchi@users.noreply.github.com>
2021-07-16 08:46:12 +0700

    Big Update:UI,Candidate get, swap json

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
-import 'package:messagepack/messagepack.dart';
 import 'package:sdp_transform/sdp_transform.dart';
 
 void main() {
@@ -33,17 +33,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool offer = false;
+  String statusObject = "";
+  bool candidateStatus = false;
   late RTCPeerConnection peerConnection;
   late MediaStream localStream;
   final localRenderer = new RTCVideoRenderer();
   final remoteRenderer = RTCVideoRenderer();
 
-  final sdpController = TextEditingController();
+  final sdpCtrler = TextEditingController();
+  final statusCtrler = TextEditingController();
+  final clipboardCtrler = TextEditingController();
+  final candidateCtrler = TextEditingController();
   @override
   void dispose() {
     localRenderer.dispose();
     remoteRenderer.dispose();
-    sdpController.dispose();
+    sdpCtrler.dispose();
     super.dispose();
   }
 
@@ -78,27 +83,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
     localStream = await getUserMedia();
 
-   RTCPeerConnection pc =
+    RTCPeerConnection pc =
         await createPeerConnection(configuration, offerSdpConstraints);
 
     pc.addStream(localStream);
 
     pc.onIceCandidate = (e) {
       if (e.candidate != null) {
-        final packer = Packer();
-        packer.packString(e.candidate);
-        packer.packString(e.sdpMid.toString());
-        packer.packString(e.sdpMlineIndex.toString());
-        final bytes = packer.takeBytes();
-        print("Message Pack convert below");
-        print(bytes);
-
-        print("Jso code candidate");
         print(json.encode({
           'candidate': e.candidate.toString(),
           'sdpMid': e.sdpMid.toString(),
           'sdpMlineIndex': e.sdpMlineIndex,
         }));
+        if (candidateCtrler.text == "")
+          setState(() {
+            statusCtrler.text = "Collected Candidate";
+            candidateStatus = true;
+            candidateCtrler.text = json.encode({
+              'candidate': e.candidate.toString(),
+              'sdpMid': e.sdpMid.toString(),
+              'sdpMlineIndex': e.sdpMlineIndex
+            });
+          });
       }
     };
 
@@ -129,26 +135,36 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void createOffer() async {
+    statusObject = "";
     RTCSessionDescription description =
         await peerConnection.createOffer({'offerToReiceveVideo': 1});
     var session = parse(description.sdp.toString());
     print(json.encode(session));
-    offer = true;
+    setState(() {
+      clipboardCtrler.text = json.encode(session);
+      offer = true;
+      statusObject = "offer";
+    });
 
     peerConnection.setLocalDescription(description);
   }
 
   void createAnswer() async {
+    statusObject = "";
     RTCSessionDescription description =
         await peerConnection.createAnswer({'offerToReiceveVideo': 1});
     var session = parse(description.sdp.toString());
     print(json.encode(session));
+    setState(() {
+      clipboardCtrler.text = json.encode(session);
+      statusObject = "answer";
+    });
 
     peerConnection.setLocalDescription(description);
   }
 
   void setRemoteDescription() async {
-    String jsonString = sdpController.text;
+    String jsonString = sdpCtrler.text;
     dynamic session = await jsonDecode('$jsonString');
 
     String sdp = write(session, null);
@@ -156,13 +172,17 @@ class _MyHomePageState extends State<MyHomePage> {
     RTCSessionDescription description =
         new RTCSessionDescription(sdp, offer ? 'answer' : 'offer');
 
+    setState(() {
+      statusCtrler.text = "Set Remote Done";
+    });
     print(description.toMap());
 
     await peerConnection.setRemoteDescription(description);
   }
 
   void setCandidate() async {
-    String jsonString = sdpController.text;
+    statusObject = "";
+    String jsonString = sdpCtrler.text;
     dynamic session = await jsonDecode('$jsonString');
     print(session['candidate']);
     dynamic candidate = new RTCIceCandidate(
@@ -193,11 +213,21 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           ElevatedButton(
-            onPressed: createOffer,
+            onPressed: () {
+              setState(() {
+                createOffer();
+                statusCtrler.text = "Collected Offer";
+              });
+            },
             child: Text('Offer'),
           ),
           ElevatedButton(
-            onPressed: createAnswer,
+            onPressed: () {
+              setState(() {
+                createAnswer();
+                statusCtrler.text = "Collected Answer";
+              });
+            },
             child: Text("Answer"),
           )
         ],
@@ -206,7 +236,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Padding sdpCandidateTF() => Padding(
         padding: EdgeInsets.all(16.0),
         child: TextField(
-          controller: sdpController,
+          controller: sdpCtrler,
           keyboardType: TextInputType.multiline,
           maxLines: 4,
           maxLength: TextField.noMaxLength,
@@ -217,13 +247,43 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           ElevatedButton(
-            onPressed: setRemoteDescription,
+            onPressed: () {
+              setRemoteDescription();
+              sdpCtrler.text = "";
+            },
             child: Text('Set Remote Description'),
           ),
-          ElevatedButton(onPressed: setCandidate, child: Text('Set Candidate'))
+          ElevatedButton(
+              onPressed: () {
+                setCandidate();
+                sdpCtrler.text = "";
+              },
+              child: Text('Set Candidate'))
         ],
       );
 
+  Container status() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Status: "),
+          Text(statusCtrler.text),
+        ],
+      ),
+    );
+  }
+
+  ElevatedButton copyClipboard() => ElevatedButton(
+        onPressed: () =>
+            Clipboard.setData(ClipboardData(text: clipboardCtrler.text)),
+        child: Text("Copy " + statusObject),
+      );
+  ElevatedButton copyCandidate() => ElevatedButton(
+        onPressed: () =>
+            Clipboard.setData(ClipboardData(text: candidateCtrler.text)),
+        child: Text("Copy Candidate"),
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -234,6 +294,14 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              status(),
+              SizedBox(height: 10.0),
+              statusObject != "" ? copyClipboard() : Container(),
+              SizedBox(height: 10.0),
+              candidateStatus && statusObject == "answer"
+                  ? copyCandidate()
+                  : Container(),
+              SizedBox(height: 10.0),
               videoRenderers(),
               offerAndAnswerButtons(),
               sdpCandidateTF(),
